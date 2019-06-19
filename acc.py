15ba316f4804ad14f7f2ea953956514b2387ef75
yudarea <wirayudpratama111@gmail.com>
2019-06-19 12:56:37 +0700

    Update acc.py

--- acc.py
+++ acc.py
@@ -3,8 +3,8 @@ import pyrebase
 from linebot import (LineBotApi, WebhookHandler)
 
 namaBot = "yudha"
-line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.environ.get('SECRET_TOKEN'))
+line_bot_api = LineBotApi(os.environ.get('BK50nyBRSOcibwYdcx4jXEM3y7ooxJ1+F0tHy86CY8IeJcn00PzfNSi1eaM5PD32fVzWWwitfz9gw/05MMUoAz5anLzleoZU1/MF5zU7ZTApopWtICcRjUuj1E0Z8Rhk+zI90vHspM0AkCuEH0ga5gdB04t89/1O/w1cDnyilFU='))
+handler = WebhookHandler(os.environ.get('6aac06ec8ec6cbcf95973a62368d08d8'))
 google_key = os.environ.get('GOOGLE_API_KEY')
 owner = 'u81a15ce8e621e30cb0f627aafc4e8b57'
 
